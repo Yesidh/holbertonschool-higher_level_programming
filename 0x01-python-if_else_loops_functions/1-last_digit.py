31b843c4d169fb1854a7098aa2dd1d46adc50205
Yesidh <944@holbertonschool.com>
2019-09-10 15:55:18 -0500

    #Feature: random number and we need to calculed the last digit and print if the number is zero, >5 or <6 and not zero

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+a = abs(number) % 10
+if a > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, a))
+elif a == 0:
+    print("Last digit of {} is {} and is 0".format(number, a))
+elif a < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, a))
