389682a1ed61ba51502dc08bf5e25a57a2bda6d1
Yesidh <944@holbertonschool.com>
2019-09-10 21:51:35 -0500

    #Fix: using the using chr

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
     if i % 2 == 1:
-        i  = i - 32
-    print("{:c}".format(i), end="")
+        i = i - 32
+    print("{}".format(chr(i)), end="")
