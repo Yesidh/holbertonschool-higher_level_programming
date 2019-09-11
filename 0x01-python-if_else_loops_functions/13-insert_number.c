0dc6e08e5b383b46995b333aa32f76875ea705ca
Yesidh <944@holbertonschool.com>
2019-09-10 21:09:48 -0500

    #Fix: betty style

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,14 @@
 #include "lists.h"
+/**
+ * insert_node - function for insert a node in the list
+ * @head: head of the list
+ * @number: number for insert in the list
+ *
+ * Return: pendiente...
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
 	if (head == NULL)
-		return(NULL);
-	return(NULL);
+		return (NULL);
+	return (NULL);
 }
