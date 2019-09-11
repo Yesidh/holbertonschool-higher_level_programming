b2828dbb4fad922d3593b7bf0fdaf43f61515872
Yesidh <944@holbertonschool.com>
2019-09-10 20:56:47 -0500

    #Fix: changing the second if by elif

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        if i % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
