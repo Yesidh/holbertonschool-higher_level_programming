0a443ce6dfa2f46126e571c44045eae91ca025c1
Yesidh <944@holbertonschool.com>
2019-09-12 17:39:32 -0500

    #Fix: adding the condition when have one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     a = int(len(sys.argv))
     if a == 1:
         print("0 arguments.")
-    elif a > 1:
+    elif a == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    elif a > 2:
         print("{:d} arguments:".format(a - 1))
         i = 1
         while i < a:
