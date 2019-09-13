5b52a187f90bcee9c6b6e58d2ae6167a72b06861
Yesidh <944@holbertonschool.com>
2019-09-13 00:00:43 -0500

    #Fix: putting the import items before the def function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    from sys import argv
     a = int(len(argv))
     if a > 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
