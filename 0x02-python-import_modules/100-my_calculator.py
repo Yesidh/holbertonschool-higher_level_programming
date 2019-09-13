c1fd7ce2c5c50ecaaaeff11f3e7c44f49099dc7f
Yesidh <944@holbertonschool.com>
2019-09-12 23:57:26 -0500

    #Fix: changing the order for the first if condition

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     a = int(len(argv))
-    b = int(argv[1])
-    c = int(argv[3])
     if a > 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    b = int(argv[1])
+    c = int(argv[3])
     elif argv[2] == '+':
         print("{:d} + {:d} = {:d}".format(b, c, int(add(b, c))))
     elif argv[2] == '-':
