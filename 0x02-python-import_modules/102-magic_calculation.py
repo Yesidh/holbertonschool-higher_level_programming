c785952b349fe4c2719fe320bec9a1ecb7c28583
Yesidh <944@holbertonschool.com>
2019-09-12 23:51:53 -0500

    #fix: pep8 style

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,6 +4,6 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c = add(c,i)
+            c = add(c, i)
         return c
     return (sub(a, b))
