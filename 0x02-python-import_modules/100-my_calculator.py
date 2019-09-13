0336e0ef101b9abdd35739e10bedfdba7e82c323
Yesidh <944@holbertonschool.com>
2019-09-12 19:56:00 -0500

    #Feature: program that imports all functions from the file calculator_1.py and handles basics operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
-    from pdb import set_trace
-    set_trace()
-    a = int(len(sys.argv))
-    operators = "+-\*"
-    b = int(sys.argv[1])
-    c = int(sys.argv[3])
+    from sys import argv
+    a = int(len(argv))
+    b = int(argv[1])
+    c = int(argv[3])
     if a > 4:
-        print("usage: {} {} {} {}".format(sys.argv[0], sys.argv[1],\
-                                          sys.argv[2], sys.argv[3]))
+        print("usage: {} {} {} {}".format(argv[0], argv[1], argv[2], argv[3]))
         exit(1)
-    for idx, val in enumerate(operators):
-        if val == sys.argv[2]:
-            if idx == 0:
-                print("{:d} + {:d} = {:d}".format(b, c, int(add(a, b))))
-                exit(0)
-            if idx == 1:
-                print("{:d} - {:d} = {:d}".format(b, c, int(sub(a, b))))
-                exit(0)
-            if idx == 2:
-                print("{:d} * {:d} = {:d}".format(b, c, mul(mul(a, b))))
-                exit(0)
+    elif argv[2] == '+':
+        print("{:d} + {:d} = {:d}".format(b, c, int(add(b, c))))
+        exit(0)
+    elif argv[2] == '-':
+        print("{:d} - {:d} = {:d}".format(b, c, int(sub(b, c))))
+        exit(0)
+    elif argv[2] == '*':
+        print("{:d} * {:d} = {:d}".format(b, c, int(mul(b, c))))
+        exit(0)
+    elif argv[2] == '/':
+        print("{:d} / {:d} = {:d}".format(b, c, int(div(b, c))))
+        exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
