c8332ab449e8396ae715222bfe083a1634b7db76
Yesidh <944@holbertonschool.com>
2019-09-13 00:05:02 -0500

    Fix: changin the condition in the first if

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
     a = int(len(argv))
-    if a > 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
     b = int(argv[1])
     c = int(argv[3])
-    if argv[2] == '+':
+    if a != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] == '+':
         print("{:d} + {:d} = {:d}".format(b, c, int(add(b, c))))
     elif argv[2] == '-':
         print("{:d} - {:d} = {:d}".format(b, c, int(sub(b, c))))
