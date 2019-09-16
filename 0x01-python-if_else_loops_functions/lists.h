bada0ad86701bf39d7da2ee036e68fbdc8092433
Yesidh <944@holbertonschool.com>
2019-09-16 00:51:06 -0500

    #Fix: adding the prototype for check_cycle

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,8 +18,10 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
+int check_cycle(listint_t *list);
 #endif
