298cd32229715e6f294ddbe7d453a7ea80203211
Yesidh <944@holbertonschool.com>
2019-09-16 00:48:32 -0500

    #Feature: funtion that checks if a singly linked list has a cycle in it

---
+++ 0x01-python-if_else_loops_functions/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - function for check if a singly linked list has a cycle in it
+ * @list: header for the single linked list
+ * Return: 0 if there is no cycly, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *w;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	w = list;
+
+	while (list->next)
+	{
+		if (w->next == list->next)
+			return (1);
+		if (list->next == NULL)
+			return (0);
+		list = list->next;
+	}
+	return (0);
+}
