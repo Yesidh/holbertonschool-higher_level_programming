b4812645712c7ae6497074b0f32470fe3d1669c8
Yesidh <944@holbertonschool.com>
2019-09-17 15:31:13 -0500

    #Feature: adding the condition if the list is not None

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-        if len(my_list) > idx and idx > 0:
-                new_list = my_list.copy()
-                new_list[idx] = element
-                return new_list
-        else:
-                return my_list
+        if my_list is not None:
+                if len(my_list) > idx and idx > 0:
+                        new_list = my_list.copy()
+                        new_list[idx] = element
+                        return new_list
+                else:
+                        return my_list
