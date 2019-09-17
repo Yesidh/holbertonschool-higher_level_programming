1a2dd64ca0279809e7ad58fccc25f3d127d10669
Yesidh <944@holbertonschool.com>
2019-09-17 15:41:52 -0500

    #Fix: adding idx >= 0 in the second if statemens

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
         if my_list is not None:
                 new_list = my_list.copy()
-                if len(new_list) > idx and idx > 0:
+                if len(new_list) > idx and idx >= 0:
                         new_list[idx] = element
                 return new_list
