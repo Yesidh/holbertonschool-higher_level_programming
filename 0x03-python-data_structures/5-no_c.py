51026db281f1c268b6d193f77f0ec6b398a8dd6a
Yesidh <944@holbertonschool.com>
2019-09-17 16:32:47 -0500

    #Fix: adding condition new_string += i for concatenate

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
         if my_string is not None:
-                my_string = [i for i in my_string if i != 'c' and i != 'C']
-        return my_string
+                new_string = ""
+                for i in my_string:
+                        if i != 'c' and i != 'C':
+                                new_string += i
+                return new_string
