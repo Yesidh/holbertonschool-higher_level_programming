04f501fd9e946b248c783fa8d23194eb25c7bdca
Yesidh <944@holbertonschool.com>
2019-09-17 15:13:00 -0500

    #Fix: adding the condition if the list have at least one element

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        for i in reversed(my_list):
-                print("{:d}".format(i))
+        if len(my_list) > 0:
+                for i in reversed(my_list):
+                        print("{:d}".format(i))
