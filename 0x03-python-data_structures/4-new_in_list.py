3be404d61556a36587a42f1fe27071d7c3d23cb7
Yesidh <944@holbertonschool.com>
2019-09-17 15:35:31 -0500

    #Fix: changing copy by [:] for copy a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
         if my_list is not None:
                 if len(my_list) > idx and idx > 0:
-                        new_list = my_list.copy()
+                        new_list = my_list[:]
                         new_list[idx] = element
                         return new_list
                 else:
