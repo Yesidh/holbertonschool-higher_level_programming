bd517a737b383abff723a42ca02075c3ff1e7500
Yesidh <944@holbertonschool.com>
2019-09-17 16:15:45 -0500

    #Fix: string like a list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,4 @@
 def no_c(my_string):
         if my_string is not None:
                 my_string = [i for i in my_string if i != 'c' and i != 'C']
-                for i in my_string:
-                        if i != 'c' and i != 'C':
-                                my_string = i
-                return my_string
+        return my_string
