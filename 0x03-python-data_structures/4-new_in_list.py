5be6d190945befab49c74590ef1519f029b54f5c
Yesidh <944@holbertonschool.com>
2019-09-17 15:39:19 -0500

    #Fix: changin condition for delete lines of code, and only one return line

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
         if my_list is not None:
-                if len(my_list) > idx and idx > 0:
-                        new_list = my_list[:]
+                new_list = my_list.copy()
+                if len(new_list) > idx and idx > 0:
                         new_list[idx] = element
-                        return new_list
-                else:
-                        return my_list
+                return new_list
