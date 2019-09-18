b05498cf63c437a6287703a9e5e70c5588810f57
Yesidh <944@holbertonschool.com>
2019-09-17 23:54:19 -0500

    #Perrada time

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints info about python
+ * @p: python pointer to objects
+ * Return: Nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
