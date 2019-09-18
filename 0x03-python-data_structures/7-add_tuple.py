f9419530b7f464d3a1c941d3c5c0ff7d9d7cac3b
Yesidh <944@holbertonschool.com>
2019-09-18 16:39:27 -0500

    #Fix: changing all logical in the function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        if not tuple_a:
-                return tuple_b
-        elif not tuple_b:
-                return tuple_a
-        tuple_ab = (sum(i) for i in zip(tuple_a, tuple_b))
-        return tuple(tuple_ab)
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    tuple_ab = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return tuple_ab
