a3222ae5e424243ad5b711e82de66c618766daf2
Yesidh <944@holbertonschool.com>
2019-09-18 18:28:47 -0500

    #Fix: changing the logical for find a loop in diferencs positions

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,25 @@
-#include "list.h"
+#include "lists.h"
+/**
+ * check_cycle - function for check if a singly linked list has a cycle in it
+ * @list: header for the single linked list
+ * Return: 0 if there is no cycly, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-	if (list == NULL)
-		return(0);
+	listint_t *w, *x;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	w = list;
+	x = list;
+
+	while (x->next->next)
+	{
+		w = w->next;
+		x = x->next->next;
+		if (w->next == x->next->next)
+			return (1);
+	}
+	return (0);
 }
