05aafbb11bdcaacb387baab1c45bf05e0c66f556
Yesidh <944@holbertonschool.com>
2019-09-19 19:17:18 -0500

    #Fix: deleting the if of len of the list, is not necesary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list is not None:
-        if len(my_list) > search:
-            new_list = my_list[:]
-            for i in range(new_list.count(search)):
-                a = new_list.index(search)
-                new_list.remove(search)
-                new_list.insert(a, replace)
-            return new_list
+        new_list = my_list[:]
+        for i in range(new_list.count(search)):
+            a = new_list.index(search)
+            new_list.remove(search)
+            new_list.insert(a, replace)
+        return new_list
