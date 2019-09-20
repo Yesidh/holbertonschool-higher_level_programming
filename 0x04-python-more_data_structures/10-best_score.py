1f8dc9b87e66982059a972ba408ba506682021d3
Yesidh <944@holbertonschool.com>
2019-09-19 23:11:39 -0500

    #Fix: adding if dictionary is zero

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary.keys()) == 0:
         return None
     a_dictionary = sorted(a_dictionary)
     max_score = 0
