c1547589c287fe9395e910f4794d4f2ece294b44
Yesidh <944@holbertonschool.com>
2019-09-19 23:21:29 -0500

    Fix: changing the logical again

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary.keys()) == 0:
         return None
-    max_score = list(sorted(a_dictionary))[-1:]
+    max_score = 0
+    a_dictionary = sorted(a_dictionary)
+    for i in a_dictionary:
+        max_score = i
     return max_score
