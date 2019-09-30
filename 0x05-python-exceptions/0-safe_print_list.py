93c41592ac5b915ad0b798659872907266e6618a
Yesidh <944@holbertonschool.com>
2019-09-30 14:46:24 -0500

    #Fix: pep8 style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if j < x:
-               print("{}".format(i), end="")
-               j += 1
+                print("{}".format(i), end="")
+                j += 1
     except IndexError:
         print("That index doesn't exist")
     print("")
