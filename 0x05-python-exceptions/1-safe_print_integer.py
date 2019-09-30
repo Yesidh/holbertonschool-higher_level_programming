88225e32aaec0e585393599cde5c1db5f815be3c
Yesidh <944@holbertonschool.com>
2019-09-30 15:49:45 -0500

    #Fix: changing the condition when the divition is >= 0

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if abs(value) / 2 >= 1:
+        if abs(value) / 2 >= 0:
             print("{:d}".format(value))
             return True
     except (TypeError, ValueError):
