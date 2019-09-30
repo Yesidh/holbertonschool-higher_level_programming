e227cbf63b9cf362e85c9c2c1be4fbccd736cddf
Yesidh <944@holbertonschool.com>
2019-09-30 15:47:28 -0500

    #Feature: Function that prints an integer, return True if a string is integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if abs(value) / 2 >= 1:
+            print("{:d}".format(value))
+            return True
+    except (TypeError, ValueError):
+            return False
