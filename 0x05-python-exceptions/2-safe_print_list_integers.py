85ba1a60e758b4565309ceee0cea075c4a041f64
Yesidh <944@holbertonschool.com>
2019-09-30 16:25:49 -0500

    #Fix: changing the condition in the for statement

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     j = 0
-    for i in my_list:
+    for i in range(0, x):
         try:
-            if abs(i) / 2 >= 0 and j < x:
-                print("{:d}".format(i), end="")
-                j += 1
-        except(IndexError, TypeError, ValueError):
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except(TypeError, ValueError):
             pass
     print("")
     return j
