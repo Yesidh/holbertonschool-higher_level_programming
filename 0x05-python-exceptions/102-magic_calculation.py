c7972fc9fdba8b433c13195a995831c40dbd959a
Yesidh <944@holbertonschool.com>
2019-10-01 00:10:43 -0500

    #Fix: changing the order the variables in the add b + a

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b / i)
         except:
-            result = a + b
+            result = b + a
             break
     return result
