c62499f43972741ab998a0bb1fbf432acd42b461
Yesidh <944@holbertonschool.com>
2019-09-30 23:27:30 -0500

    #Feature: Function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+      result = fct(*args)
+    except Exception as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
+    return result
