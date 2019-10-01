6acbd00514461cfaf37af0c328a86f1387309b08
Yesidh <944@holbertonschool.com>
2019-10-01 00:04:26 -0500

    #Fix: adding break condition and deletin ()

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += (a ** b / i)
+                result += a ** b / i
         except:
             result = a + b
+            break
     return result
