5ca125ae137fea40e3e5b1221b5e9cbaa44cfe4c
Yesidh <944@holbertonschool.com>
2019-09-30 23:57:44 -0500

    #Fix: changing the file name

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(0, 3):
+        try:
+           if i > a:
+               raise Exception('Too far')
+           else:
+               result += a ** b/ i
+        except:
+            result = a + b
+    return result
