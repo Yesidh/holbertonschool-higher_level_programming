f1dcad3181ed90fc91f97b0a76bab709e51279fc
Yesidh <944@holbertonschool.com>
2019-09-30 23:59:54 -0500

    #Fix: pep8 style

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,10 +3,10 @@ def magic_calculation(a, b):
     result = 0
     for i in range(0, 3):
         try:
-           if i > a:
-               raise Exception('Too far')
-           else:
-               result += a ** b/ i
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
         except:
             result = a + b
     return result
