83aad1797f8d14889924475cd31b87002f3a06d1
Yesidh <944@holbertonschool.com>
2019-10-01 23:49:11 -0500

    #Fix: deleting not in comparition when value[0] and value[1] should be < 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not value[0] < 0:
+        elif  value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not value[1] < 0:
+        elif  value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
