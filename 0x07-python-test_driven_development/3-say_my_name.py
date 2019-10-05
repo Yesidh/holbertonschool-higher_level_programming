0c45be25d95899d0c7739225052c90e9043d3cf3
Yesidh <944@holbertonschool.com>
2019-10-05 11:18:23 -0500

    #Fix: adding :s in the print

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,4 @@ def say_my_name(first_name, last_name=""):
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {:s} {:s}".format(first_name, last_name))
