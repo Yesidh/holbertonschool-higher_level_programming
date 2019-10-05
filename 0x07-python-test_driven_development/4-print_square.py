aa7d91f667deef1aeeb49ce8736f8aae75d7d42e
Yesidh <944@holbertonschool.com>
2019-10-04 20:45:26 -0500

    Feature: function that print a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+===============================
+Module for print_square function
+===============================
+"""
+
+
+def print_square(size):
+    """Function that prints a square with "#" character with
+    hight and width like "size" argument"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(0, size):
+            print("#" * size, end="")
+            print()
