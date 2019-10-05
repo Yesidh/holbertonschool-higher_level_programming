e228c542382ed5667f6b537e18ce90cea916ec37
Yesidh <944@holbertonschool.com>
2019-10-04 20:44:37 -0500

    Fix: pep8 style

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,10 +4,12 @@
 Calculation module
 ****************************
 """
+
+
 def add_integer(a, b=98):
     """ Addition Function"""
 
-    if  not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
