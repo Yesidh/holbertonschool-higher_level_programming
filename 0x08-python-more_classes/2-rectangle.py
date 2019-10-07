a4c17caa61d4f27f1849c88a5ae815b36852fd50
Yesidh <944@holbertonschool.com>
2019-10-07 17:30:58 -0500

    #Fix: adding the condition if widght or height is equal to zero

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,7 +39,13 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
