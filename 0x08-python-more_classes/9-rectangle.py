f68cddcaee5a2896252cc1dea0a7b4fa9238fabc
Yesidh <944@holbertonschool.com>
2019-10-07 22:40:10 -0500

    #Fix: adding () in the return classmethod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return (cls(size, size))
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
