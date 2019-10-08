c65b4803e7a212cea513cbded8bb8fc4e5b6ab0b
Yesidh <944@holbertonschool.com>
2019-10-07 23:20:55 -0500

    #Fix : adding all the file again

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
         return matriz
 
     def __repr__(self):
-        return "Rectangle({:s}, {:s})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
@@ -75,11 +75,11 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+        elif rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_2.area() > rect_1.area():
+        elif rect_1.area() < rect_2.area():
             return rect_2
 
     @classmethod
