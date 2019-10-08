c667d0ec72ccf68e10d54d6ecc59fa891e42ece3
Yesidh <944@holbertonschool.com>
2019-10-07 19:09:28 -0500

    #Fix: deleting __ in the variables in __init__

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,8 +9,8 @@ Mudele with module class Rectangle
 class Rectangle:
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
