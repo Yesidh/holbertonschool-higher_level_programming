53b6a3f0d24f41a37b1a4b06ca52958bd2ebc9b0
Yesidh <944@holbertonschool.com>
2019-10-12 11:16:58 -0500

    #Fix: changing the raise error when size is < 0:

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
-        raise TypeError("size must be an integer")
+        raise TypeError("size must be >= 0")
     else:
         for i in range(0, size):
             print("#" * size)
