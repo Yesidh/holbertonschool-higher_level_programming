4e52fe700ad4b922c5d8bd9e6189327467857a5f
Yesidh <944@holbertonschool.com>
2019-10-15 18:14:56 -0500

    #Fix: deleting BaseGeometry class and addint this with import

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,22 +6,7 @@ module with class BaseGeometry
 """
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """method for calculated area"""
-
-        raise Exception("area () is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method for validate if a num is integer"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greatter than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
