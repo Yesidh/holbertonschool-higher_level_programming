c5d6ff8b369ac32144f3e38af939de71a3de4f21
Yesidh <944@holbertonschool.com>
2019-10-15 18:50:27 -0500

    #Fix: pep8

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
 """
 ===================================
 module with class BaseGeometry
 ===================================
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
 
     def __init__(self, size):
         """Method for initialized the attrubutes"""
