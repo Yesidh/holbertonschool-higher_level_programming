6ed3d55dcb275cb3c217877dcf81f0a9cb42ce21
Yesidh <944@holbertonschool.com>
2019-10-15 20:41:56 -0500

    #Fix: pep8 style

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
 class MyList(list):
+    """Class with method print_sorted"""
+
     def print_sorted(self):
+        """Methot that sorted a list"""
+
         new_list = self[:]
         print(sorted(new_list))
