59d963c07e46c8c7ca199487e4e01d5377abf818
Yesidh <944@holbertonschool.com>
2019-10-16 15:41:26 -0500

    #Feature: Function that read n lines of a text file and prints it to stdout

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    """function that reads a text and return number of lines"""
+    lines = 0
+    with open(filename) as f:
+        for lin in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines <= 0 or nb_lines >= lines:
+            print(f.read(), end="")
+        else:
+            for i in range(0, nb_lines):
+                print(f.readline(), end="")
