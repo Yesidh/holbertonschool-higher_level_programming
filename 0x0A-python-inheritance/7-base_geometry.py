bbf30bad7361e5e01e7ab4af350300fb45f7dd4a
Yesidh <944@holbertonschool.com>
2019-10-16 00:05:53 -0500

    #Fix: deleting a t in gratter

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,4 +19,4 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greatter than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
