cdbbd6cbb91110cf722654a2a0399633fb89a7fc
Yesidh <944@holbertonschool.com>
2019-11-25 17:36:12 -0500

    [Fix]: changing the syntax

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS
 second_table (id INT, name VARCHAR(256), score INT);
 
-INSERT INTO second_table (id, name, score)
+INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES
 (1, "Jhon", 10),
 (2, "Alex", 3),
