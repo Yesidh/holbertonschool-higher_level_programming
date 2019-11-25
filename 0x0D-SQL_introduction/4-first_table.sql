07e1d3a4a872127ba8597f7a4d8d499901fede77
Yesidh <944@holbertonschool.com>
2019-11-25 15:22:35 -0500

    [Feature]: script that creates a table called first_table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- script that creates a table called first_table in the current database in
+-- Mysql server, id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256))
