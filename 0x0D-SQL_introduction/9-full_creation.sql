e42dc7197462dc44dbf58def88bb1881ede9b85e
Yesidh <944@holbertonschool.com>
2019-11-25 17:25:07 -0500

    [Fix]: changing the syntax

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,10 @@
 -- Mysql server, second_table description:
 -- id INT, name VARCHAR(256), score INT
 -- with this records:(1, John, 10), (2, Alex, 3), (3, Bob, 14) y (4, George, 8)
-CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS
+second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256),  score INT);
-INSERT INTO second_table (id, name, score) VALUES ('1', 'Jhon', '10');
-INSERT INTO second_table (id, name, score) VALUES ('2', 'Alex', '3');
-INSERT INTO second_table (id, name, score) VALUES ('3', 'Bob', '14');
-INSERT INTO second_table (id, name, score) VALUES ('4', 'George', '8');
+INSERT INTO second_table (id, name, score) VALUES ('1', 'Jhon', '10'),
+(2, 'Alex', 3);
+(3, 'Bob', 14);
+(4, 'George', 8);
