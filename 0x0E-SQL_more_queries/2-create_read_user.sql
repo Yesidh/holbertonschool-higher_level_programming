39eb54c6f76759b5c7a1198bb0ff0d286a67b7b8
Yesidh <944@holbertonschool.com>
2019-11-26 18:17:42 -0500

    [Feature]: script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
