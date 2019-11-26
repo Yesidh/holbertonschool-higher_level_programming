80a8dff84d85a6598eee8992d2139f47504c8b47
Yesidh <944@holbertonschool.com>
2019-11-25 23:49:31 -0500

    [Feature]: script that display the average temperature by city ordered by descending temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- mysql -u root -p hbtn_0c_0 < /home/yesid/Downloads/temperatures.sql whit this command
+-- import the table into my databse
+-- Now I need a script that displays the average temperature by city ordered by temperature
+-- descending
+SELECT city AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
