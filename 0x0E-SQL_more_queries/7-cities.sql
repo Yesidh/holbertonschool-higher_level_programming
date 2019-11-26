90226d0a28d99d6fd372aabd227b8d3468868b4c
Yesidh <944@holbertonschool.com>
2019-11-26 18:58:39 -0500

    [Fix]: deleting ; in previous last line

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,9 +5,10 @@
 --      name VARCHAR(256) can’t be null
 --    If the database hbtn_0d_usa already exists, your script should not fail
 --    If the table cities already exists, your script should not fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL,
-name VARCHAR(256) NOT NULL;
+name VARCHAR(256) NOT NULL,
 FOREIGN KEY(state_id) REFERENCES states(id));
