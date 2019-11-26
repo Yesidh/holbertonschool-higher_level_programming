2088fcbb6e96c1479b204b7de42ea7cbbba1cab1
Yesidh <944@holbertonschool.com>
2019-11-25 23:07:29 -0500

    [Fix]: changing general by unicode

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- script that converts hbtn_0c_0 database to UTF8(utf8mb4, collate utf8mb4_unicode_ci)
 -- in Mysql server
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
