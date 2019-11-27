7c31e512f760265b2226c40a98e2b6c62604f12e
Yesidh <944@holbertonschool.com>
2019-11-26 23:18:08 -0500

    [Fix]: Changing ON condition

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,8 +3,9 @@
 --   Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --   You can use only one SELECT statement
 --   The database name will be passed as an argument of the mysql command
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
