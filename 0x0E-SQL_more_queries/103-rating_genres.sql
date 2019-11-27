c09037633b23d2ca6cf2ba1cffe2c2b82c11d330
Yesidh <944@holbertonschool.com>
2019-11-26 23:48:03 -0500

    [perrada time]

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,6 @@
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+--    Each record should display: tv_genres.name - rating sum
+--    Results must be sorted in descending order by their rating
+--    You can use only one SELECT statement
+--    The database name will be passed as an argument of the mysql command
+IGNORE ALL;
