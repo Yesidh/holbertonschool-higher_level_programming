8715ed1d771f11b9eed8e6e8cd60941a99c87490
Yesidh <944@holbertonschool.com>
2019-11-26 23:44:42 -0500

    [perrada time]

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+--    Each record should display: tv_shows.title - rating sum
+--    Results must be sorted in descending order by the rating
+--    You can use only one SELECT statement
+--    The database name will be passed as an argument of the mysql command
+IGNORE ALL;
