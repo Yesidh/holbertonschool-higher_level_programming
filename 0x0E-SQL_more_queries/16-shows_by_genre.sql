73b1faf2fc6a1e3342885267a0f9966c3b511437
Yesidh <944@holbertonschool.com>
2019-11-26 23:38:39 -0500

    [perrada time]

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+--    If a show doesn’t have a genre, display NULL in the genre column
+--    Each record should display: tv_shows.title - tv_genres.name
+--    Results must be sorted in ascending order by the show title and genre name
+--    You can use only one SELECT statement
+--    The database name will be passed as an argument of the mysql command
+IGNORE ALL;
