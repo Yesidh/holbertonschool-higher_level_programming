e5adbfc10ae535b4624f6a811bef31a10d50b28c
Yesidh <944@holbertonschool.com>
2019-11-27 01:29:14 -0500

    [Fix]: adding the condition WHERE and changind RIGHT by LEFT

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,7 +3,10 @@
 --    Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-SELECT
-FROM
-ON
-ORDER BY;
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
