221ccf48422648a77296203b5264619a0834d3a3
Yesidh <944@holbertonschool.com>
2019-11-27 01:10:41 -0500

    [Fix]: script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,7 +4,9 @@
 --   If a show doesn’t have a genre, display NULL
 --   You can use only one SELECT statement
 --   The database name will be passed as an argument of the mysql command
-SELECT
-FROM
-ON
-ORDER BY;
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
