0ff2b1428a90b7247b55f10e2a374e0939d69f44
Yesidh <944@holbertonschool.com>
2019-11-26 23:42:43 -0500

    [perrada time]

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+--    The tv_genres table contains only one record where name = Comedy (but the id can be different)
+--    Each record should display: tv_shows.title
+--    Results must be sorted in ascending order by the show title
+--    You can use a maximum of two SELECT statement
+--    The database name will be passed as an argument of the mysql command
+IGNORE ALL;
