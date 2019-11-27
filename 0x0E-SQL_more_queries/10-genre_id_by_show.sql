5eca3d914ff8fe63e5d1711cf385d98fba07e730
Yesidh <944@holbertonschool.com>
2019-11-26 23:16:02 -0500

    [Fix]: addin ORDER BY condition

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.id;
+ON tv_shows.id = tv_show_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
