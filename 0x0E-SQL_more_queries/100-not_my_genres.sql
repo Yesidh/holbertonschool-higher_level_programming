4d0fb7bde568d70636076df9f46bc33c656a5570
Yesidh <944@holbertonschool.com>
2019-11-26 23:40:29 -0500

    [perrada time]

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,7 @@
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+--    The tv_shows table contains only one record where title = Dexter (but the id can be different)
+--    Each record should display: tv_genres.name
+--    Results must be sorted in ascending order by the genre name
+--    You can use a maximum of two SELECT statement
+--    The database name will be passed as an argument of the mysql command
+IGNORE ALL;
