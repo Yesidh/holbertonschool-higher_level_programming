8640807f26d58560eeaaca1c53a51ef60a3e9638
Yesidh <944@holbertonschool.com>
2019-11-27 22:35:45 -0500

    [Feature]: script that lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,12 @@
 --    Results must be sorted in ascending order by the show title
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-IGNORE ALL;
+
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
