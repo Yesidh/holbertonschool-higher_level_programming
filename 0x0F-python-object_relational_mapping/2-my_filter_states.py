bb5f245325a85c04a8ddb67e67de67ce2f0a10d7
Yesidh <944@holbertonschool.com>
2019-12-16 03:57:06 -0500

    [Fix]: deleting comments and empty lines

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,10 +12,6 @@ database name and state name searched
 import MySQLdb
 import sys
 
-# db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-# the way obove is how to connect a database using MySQLdb in this case we need
-# to add the conection port
-
 
 def _States():
     """states to import"""
@@ -24,17 +20,13 @@ connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3],
                              port=3306)
 
-test = sys.argv[4]
 cur = connect_db.cursor()  # cur is a method to retrieve data from database
-que = "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(test)
+que = "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(sys.argv[4])
 cur.execute(que)
 rows = cur.fetchall()  # save the query in the rows variable like cur especifi
-
 for row in rows:
     print(row)
-
 cur.close()  # close all cursors
 connect_db.close()  # close databases
-
 if __name__ == "__main__":
     _States()
