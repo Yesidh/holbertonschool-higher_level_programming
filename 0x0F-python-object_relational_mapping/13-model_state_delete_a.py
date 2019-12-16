301e60ab0ed046c37806dc87f44d23caed742191
Yesidh <944@holbertonschool.com>
2019-12-15 23:21:40 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+============================================================================
+script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
+============================================================================
+"""
+
+from model_state import Base, State
+import sqlalchemy
+import sys
+
+
+def _states():
+    """states to import"""
+
+if __name__ == "__main__":
+    _states()
