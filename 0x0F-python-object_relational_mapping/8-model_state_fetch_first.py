9567b41a7c586f4bdc2b0f10dc833ab50cde737f
Yesidh <944@holbertonschool.com>
2019-12-15 23:05:50 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+=========================================================================
+script that prints the first State object from the database hbtn_0e_6_usa
+=========================================================================
+"""
+
+from model_state import Base, State
+import sqlalchemy
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
