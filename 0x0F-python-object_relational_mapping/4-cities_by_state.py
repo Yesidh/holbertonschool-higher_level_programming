2aa8322e82db95a55ae7ccf477fbce03413ffdb7
Yesidh <944@holbertonschool.com>
2019-12-15 22:47:08 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================================================
+script that lists all cities from the database hbtn_0e_4_usa
+============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
