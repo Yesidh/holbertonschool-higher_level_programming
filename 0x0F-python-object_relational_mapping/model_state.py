41ca816ffa75fbe4d7ff545121dd633e735c5d8d
Yesidh <944@holbertonschool.com>
2019-12-15 22:51:43 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+===============================================================
+ python file that contains the class definition of a State and
+ an instance Base = declarative_base()
+===============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
