de8c30d1b5bba2937ccd01f9558aa1583cdb2f9e
Yesidh <944@holbertonschool.com>
2019-12-15 23:28:20 -0500

    [perrada time]

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base= declarative_base()
+Base = declarative_base()
 
 
 def City(Base):
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,7 +10,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base= declarative_base()
+Base = declarative_base()
 
 
 def State():
