bec011bc56ef3246a65ea5859b4560b3be666ee9
Yesidh <944@holbertonschool.com>
2019-12-15 22:27:22 -0500

    [Perrada time]

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+===========================================================
+script that lists all states from the database hbtn_0e_0_usa
+============================================================
+"""
+
+
+import MySQLdb
+import sys
+
+
+
+
+
+
+if __name__ == "__main__":
