303eb520ac5d62b9fbbc95fee367749f02e5f93c
Yesidh <944@holbertonschool.com>
2019-12-15 22:48:56 -0500

    [Perrada time]

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+===============================================================
+script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+===============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
