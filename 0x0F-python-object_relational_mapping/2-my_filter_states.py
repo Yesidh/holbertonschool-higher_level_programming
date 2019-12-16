018e039626a5f61b3c037d4fd64bd5f6569cc30b
Yesidh <944@holbertonschool.com>
2019-12-16 11:59:24 -0500

    [Fix]: identation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,18 +16,18 @@ import sys
 def _States():
     """states to import"""
 
-connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3],
                              port=3306)
 
-cur = connect_db.cursor()  # cur is a method to retrieve data from database
-cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;"
+    cur = connect_db.cursor()  # cur is a method to retrieve data from database
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;"
             .format(sys.argv[4]))
-rows = cur.fetchall()  # save the query in the rows variable like cur especifi
-for row in rows:
-    if row[1] == sys.argv[4]:
-        print(row)
-cur.close()  # close all cursors
-connect_db.close()  # close databases
+    rows = cur.fetchall()  # save the query in the rows variable like cur especifi
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()  # close all cursors
+    connect_db.close()  # close databases
 if __name__ == "__main__":
     _States()
