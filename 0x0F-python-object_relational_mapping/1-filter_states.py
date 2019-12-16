58700fdf3ea338c647b41508f221ab35456573ff
Yesidh <944@holbertonschool.com>
2019-12-15 22:39:46 -0500

    [perrada time]:

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+===========================================================
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
