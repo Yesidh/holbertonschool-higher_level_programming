61b84dfe970cefdd442a8c0d0b1de678e4e28ce5
Yesidh <944@holbertonschool.com>
2019-12-15 22:45:25 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================================================
+SQL injection
+============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
