9a89f0c17f8cd7c33dd661888b66ee2025b2693c
Yesidh <944@holbertonschool.com>
2019-12-15 23:20:10 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+============================================================================
+script that changes the name of a State object from the database hbtn_0e_6_usa
+============================================================================
+"""
+
+from model_state import Base, State
+import sqlalchemy
+import sys
+
+
+def _states():
+    """states to import"""
+
+if __name__ == "__main__":
+    _states()
