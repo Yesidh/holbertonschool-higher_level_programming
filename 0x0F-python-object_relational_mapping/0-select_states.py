2738989581814a526f1a33de103dc4716a5aa2ad
Yesidh <944@holbertonschool.com>
2019-12-16 02:33:32 -0500

    [Feature]: script that lists all states from database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,6 +2,8 @@
 """
 ===========================================================
 script that lists all states from the database hbtn_0e_0_usa
+using arguments from the script: should take 3 arguments:
+ mysql username, mysql password and database name
 ============================================================
 """
 
@@ -9,8 +11,24 @@ import MySQLdb
 import sys
 
 
-def states():
+# db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+# the way obove is how to connect a database using MySQLdb in this case we need
+# to add the conection port
+
+def _States():
     """states to import"""
 
+    connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3],
+                                 port=3306)
+
+    cur = connect_db.cursor()  # cur is a method to retieve data from database
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()  # close all cursors
+    connect_db.close()  # close database
+
 if __name__ == "__main__":
-    states()
+    _States()
