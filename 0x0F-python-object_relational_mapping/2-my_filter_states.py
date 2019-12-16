0d2b2eed82c7539c32367ac38cc81def80810f14
Yesidh <944@holbertonschool.com>
2019-12-16 03:00:45 -0500

    [Feature]: script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,6 +3,8 @@
 ==============================================================
 script that takes in an argument and displays all values in the
 states table of hbtn_0e_0_usa where name matches the argument
+should take 4 arguments: mysql username, mysql password,
+database name and state name searched
 ==============================================================
 """
 
@@ -10,8 +12,21 @@ import MySQLdb
 import sys
 
 
-def states():
+def _States():
     """states to import"""
 
+connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3],
+                             port=3306)
+
+cur = connect_db.cursor()  # cur is a method to retrieve data from database
+cur.execute("SELECT * FROM states ORDER BY id;")
+rows = cur.fetchall()  # save the query in the rows variable like cur especifi
+for row in rows:
+    if row[1] == sys.argv[4]:
+        print(row)
+cur.close()  # close all cursors
+connect_db.close()  # close databases
+
 if __name__ == "__main__":
-    states()
+    _States()
