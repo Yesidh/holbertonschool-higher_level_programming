677b4888a14e90cefb2e4eb8be58e1e45c498c25
Yesidh <944@holbertonschool.com>
2019-12-15 23:13:27 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+======================================================================
+script that lists all State objects that contain the letter a from the
+database hbtn_0e_6_usa
+======================================================================
+"""
+
+from model_state import Base, State
+import sqlalchemy
+import sys
+
+
+def _states():
+    """states to import"""
+
+if __name__ == "__main__":
+    _states()
