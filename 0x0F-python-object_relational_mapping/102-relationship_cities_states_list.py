0a92613d74ef11f485fa3a36c207dcac44d56609
Yesidh <944@holbertonschool.com>
2019-12-15 23:44:48 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+============================================================================
+102-relationship_cities_states_list.py
+============================================================================
+"""
+
+from model_state import Base, State
+import sqlalchemy
+import sys
+
+
+def _states():
+    """states to import"""
+
+if __name__ == "__main__":
+    _states()
