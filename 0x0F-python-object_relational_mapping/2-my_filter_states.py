7e607ea844f70c2d4d56b1ad56a3b50761e67e74
Yesidh <944@holbertonschool.com>
2019-12-15 22:41:52 -0500

    [perrada time]

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+==============================================================
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
+==============================================================
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """states to import"""
+
+if __name__ == "__main__":
+    states()
