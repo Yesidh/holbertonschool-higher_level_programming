a3e52b409a37f9fe9f9609b2b4b1c21cc437d772
Yesidh <944@holbertonschool.com>
2019-12-16 03:41:36 -0500

    [Fix]: adding some comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,13 @@ database name and state name searched
 ==============================================================
 """
 
+
 import MySQLdb
 import sys
+
+# db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+# the way obove is how to connect a database using MySQLdb in this case we need
+# to add the conection port
 
 
 def _States():
@@ -19,8 +24,8 @@ connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3],
                              port=3306)
 
-que = "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(sys.argv[4])
 cur = connect_db.cursor()  # cur is a method to retrieve data from database
+que = "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(sys.argv[4])
 cur.execute(que)
 rows = cur.fetchall()  # save the query in the rows variable like cur especifi
 
