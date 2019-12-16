568994c94ace2b7be69796c6d30737fed2c6fe92
Yesidh <944@holbertonschool.com>
2019-12-16 14:08:55 -0500

    [Feature]: prevent SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,5 +12,16 @@ import sys
 def states():
     """states to import"""
 
+    connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = connect_db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='%s';" % sys.argv[4])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    connect_db.close()
+
 if __name__ == "__main__":
     states()
