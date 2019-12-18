db67291f28a89853f041a94154b5e04c76ffdf87
Yesidh <944@holbertonschool.com>
2019-12-17 22:36:53 -0500

    [Fix]: script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,5 +13,19 @@ import sys
 def states():
     """states to import"""
 
+    connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3], port=3305)
+
+    cur = connect_db.cursor()
+    query1 = "SELECT cities.name FROM cities INNER JOIN states ON\
+    states.id = cities.state_id WHERE states.name = %s"
+    cur.execute(query1, (sys.argv[4], ))
+    val = cur.fetchall()
+    _list = []
+    for row in val:
+        _list.append(row[0])
+    print(', '.join(_list))
+    cur.close()
+    connect_db.close()
 if __name__ == "__main__":
     states()
