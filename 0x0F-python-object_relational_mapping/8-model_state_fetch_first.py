7d87897ba5b779ed05c997428f67f0e0579d0be7
Yesidh <944@holbertonschool.com>
2019-12-18 00:44:42 -0500

    [Fix]: script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,12 +6,17 @@ script that prints the first State object from the database hbtn_0e_6_usa
 """
 
 from model_state import Base, State
-import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
 import sys
 
-
-def _states():
-    """states to import"""
-
 if __name__ == "__main__":
-    _states()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    the_session = sessionmaker(bind=engine)
+    session = the_session()
+
+    el_primerito = session.query(State).first()
+    print("{}: {}".format(el_primerito.id, el_primerito.name))
