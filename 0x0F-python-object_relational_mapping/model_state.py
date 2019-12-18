bcf76438f3528f2c764da56a634dd545a522161f
Yesidh <944@holbertonschool.com>
2019-12-17 23:51:23 -0500

    [ Fix]: writing class atributes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,8 +13,13 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-def State():
-    """states to import"""
+class State(Base):
+    """a class"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     State()
