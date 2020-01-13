b12b1f05279745327dd45f9120eba73e4f64d28d
Yesidh <944@holbertonschool.com>
2020-01-13 15:33:44 -0500

    [Feature]: python script that fetches htts://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3                                                                       
+# Write a Python script that fetches https://intranet.hbtn.io/status
+#    You must use the package urllib
+#    You are not allowed to import any packages other than urllib
+#    The body of the response must be displayed like the following example (tabulation before -)
+#     You must use a with statement
+
+import urllib.request
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+
+print('Body response:')
+print('\t- type:', type(the_page))
+print('\t- content:', the_page)
+print('\t- utf8 content:', the_page.decode('utf8'))
