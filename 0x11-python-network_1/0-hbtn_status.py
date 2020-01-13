ca013a48ce710dd46e5c5611d142070ea06c965b
Yesidh <944@holbertonschool.com>
2020-01-13 15:42:33 -0500

    [Fix]: pepep8 style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3                                                                       
+#!/usr/bin/python3
 # Write a Python script that fetches https://intranet.hbtn.io/status
 #    You must use the package urllib
 #    You are not allowed to import any packages other than urllib
-#    The body of the response must be displayed like the following example (tabulation before -)
+#    The body of the response must be displayed like the following example
 #     You must use a with statement
 
 import urllib.request
