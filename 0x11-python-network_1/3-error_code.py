2b492d919c3e6d854d17fb80d31102d769cc1f49
Yesidh <944@holbertonschool.com>
2020-01-13 18:27:14 -0500

    [Fix]: pep8 style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf 8'))
     except urllib.error.HTTPError as e:
-        print('Errore code:', e.code)
+        print('Error code:', e.code)
