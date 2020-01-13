a02bb2a78d4203cd4485511c9bd941b7800b0185
Yesidh <944@holbertonschool.com>
2020-01-13 16:16:05 -0500

    [Fix]: adding if__name__ == __main__:

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,6 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
