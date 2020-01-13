ab7b919e86185870027c78bc4dc719e6c8ec1024
Yesidh <944@holbertonschool.com>
2020-01-13 18:59:38 -0500

    [Feature]:

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
