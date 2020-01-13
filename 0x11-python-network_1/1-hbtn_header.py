a83e969ba33fe57f893c67b210a02aadab9d2bde
Yesidh <944@holbertonschool.com>
2020-01-13 16:24:02 -0500

    [Fix]: pep8 style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Python script that takes in a URL, sends a request to the URL and displays the
-# value of the X-Request-Id variable found in the header of the response.
+# Python script that takes in a URL, sends a request to the URL and displays
+# the value of the X-Request-Id variable found in the header of the response.
 #    You must use the packages urllib and sys
 #    You are not allow to import packages other than urllib and sys
 #    The value of this variable is different for each request
