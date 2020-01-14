37056913769181ba278f54cdac18864742eac9d5
Yesidh <944@holbertonschool.com>
2020-01-13 19:01:14 -0500

    [Feature]:  Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+#     You must use the package requests
+#     You are not allow to import packages other than requests
+#     The body of the response must be display like the following example
+#         (tabulation before -)
+
 import requests
 
 if __name__ == "__main__":
