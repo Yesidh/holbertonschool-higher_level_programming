bc657d379d8fede21275f292ffe66bb4616c0b98
Yesidh <944@holbertonschool.com>
2020-01-14 21:16:03 -0500

    [Feature]: list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# list 10 commits (from the most recent to oldest) of the repository
+# “rails” by the user “rails”
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url1 = '/' + sys.argv[1] + '/' + sys.argv[2] + '/commits'
+    url = 'https://api.github.com/repos' + url1
+    r = requests.get(url)
+    for i in range(0, 10):
+        print(r.json()[i].get('sha') + ': ', end="")
+        print(r.json()[i].get('commit').get('author').get('name'))
