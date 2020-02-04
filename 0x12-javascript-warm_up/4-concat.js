83e466a8c937e2b532b35917b4f0ab64beda4947
Yesidh <944@holbertonschool.com>
2020-02-04 17:26:48 -0500

    [Feature]: script that prints two arguments passed to it, in the following format: is

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* script that prints the first argument passed to it:
+      If no arguments are passed to the script, print “No argument”
+      You must use console.log(...) to print all output
+      You are not allowed to use var
+      You are not allowed to use length
+*/
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
