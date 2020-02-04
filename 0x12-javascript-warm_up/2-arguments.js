a11d00195256c9e9f3cf4e2491f5fbc57881091b
Yesidh <944@holbertonschool.com>
2020-02-04 16:50:53 -0500

    [Fix]: semistandar sytle

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-/*script that prints a message depending of the number of arguments passed:
+/* script that prints a message depending of the number of arguments passed:
   If no arguments are passed to the script, print “No argument”
   If only one argument is passed to the script, print “Argument found”
   Otherwise, print “Arguments found”
 */
 
 if (process.argv.length > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
 if (process.argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 }
 if (process.argv.length === 2) {
-    console.log('No argument');
+  console.log('No argument');
 }
