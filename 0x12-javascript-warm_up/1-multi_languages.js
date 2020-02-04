ad2373abf2060a1317617ef67c2ee7b14efbae6f
Yesidh <944@holbertonschool.com>
2020-02-04 15:41:45 -0500

    [Feature]: script tha print 3 string using console.log()

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* script that prints 3 lines:
+    'C is fun'
+    'Python is cool'
+    'Javascript is amazing'
+*/
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
