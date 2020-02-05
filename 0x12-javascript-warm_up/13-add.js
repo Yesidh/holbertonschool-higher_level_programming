3ffab7d691c1263b8a8900ac247899400545bced
Yesidh <944@holbertonschool.com>
2020-02-04 23:31:01 -0500

    [Feature]: script with a funcion to will be export

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*  function that returns the addition of 2 integers.
+        The function must be visible from outside
+        The name of the function must be add
+        You are not allowed to use var
+*/
+
+exports.add = function add (a, b) {
+  return a + b;
+};
