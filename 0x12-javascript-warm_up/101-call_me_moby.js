61adabb61e0de63cc283f94c88fd108ddb551ee5
Yesidh <944@holbertonschool.com>
2020-02-05 00:08:44 -0500

    [Fix]: unction that executes x times a function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -4,3 +4,8 @@
      Prototype: function (x, theFunction)
      You are not allowed to use var
 */
+exports.callMeMoby = function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
