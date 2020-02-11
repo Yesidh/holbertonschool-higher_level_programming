0fe73401cf21453320b82961eb738bd9c13c4b7f
Yesidh <944@holbertonschool.com>
2020-02-11 01:01:25 -0500

    [Feature]: clas Square that inherits from Square int 5 file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,32 +1,27 @@
 #!/usr/bin/node
-/*
-A class Rectangle that defines a rectangle:
-    You must use the class notation for defining your class
-    The constructor must take 2 arguments: w and h
-    Initialize the instance attribute width with the value of w
-    Initialize the instance attribute height with the value of h
-    If w or h is equal to 0 or not a positive integer, create an empty object
-    Create an instance method called print() that prints the rectangle using
-      the character X
-*/
 
-class Rectangle {
-  constructor (w, h) {
-    if (w * h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+const square = require('./5-square');
+
+// inherint from Square
+class Square extends square {
+  constructor (size) {
+    super(size, size);
   }
 
-  print () {
+  // method that prints the rectangle using the character c
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     let ToPrint;
     for (let i = 0; i < this.height; i++) {
       ToPrint = '';
       for (let j = 0; j < this.width; j++) {
-        ToPrint += 'X';
+        ToPrint += c;
       }
       console.log(ToPrint);
     }
   }
 }
-module.exports = Rectangle;
+
+module.exports = Square;
