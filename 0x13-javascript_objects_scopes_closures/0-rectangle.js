1ef24981fed37519ac17b9ae50635fb2779c6d7c
Yesidh <944@holbertonschool.com>
2020-02-10 21:37:16 -0500

    [Feature]: empty class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Write an empty class Rectangle that defines a rectangle:
+     You must use the class notation for defining your class
+*/
+
+class Rectangle {}
+module.exports = Rectangle;
