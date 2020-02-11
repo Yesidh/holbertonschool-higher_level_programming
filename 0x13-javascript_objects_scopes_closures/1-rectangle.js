2df926f48c35654a6e87523f2d54a75f8ed4e1e8
Yesidh <944@holbertonschool.com>
2020-02-10 21:42:27 -0500

    [Feature]: class Rectangle with a constructor with two parameters

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+A class Rectangle that defines a rectangle:
+    You must use the class notation for defining your class
+    The constructor must take 2 arguments w and h
+    Initialize the instance attribute width with the value of w
+    Initialize the instance attribute height with the value of h
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
