34b812d76a10675d4502cedcc20332d1ea5255b6
Yesidh <944@holbertonschool.com>
2020-02-11 00:16:41 -0500

    [Fix]: deleting all comments

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-/*
-A class Square that defines a square and inherits from Rectangle of
-    4-rectangle.js:
-  You must use the class notation for defining your class and extends
-  The constructor must take 1 argument: size
-  The constructor of Rectangle must be called (by using super())
-*/
 
+const Rectangle = require('./4-rectangle');
+
+//inherint from Rectangle
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
