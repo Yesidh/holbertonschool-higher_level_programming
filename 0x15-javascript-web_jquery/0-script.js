c653264ee247a2a6b3c979375de196a3f199b761
Yesidh <944@holbertonschool.com>
2020-02-13 20:08:13 -0500

    [Feature]: script that updates the text color of the HTML tag HEADER to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+//script that updates the text color of the HTML tag HEADER to red (#FF0000)
+//forma 1
+document.querySelector('header').style.color = '#FF0000';
+/*
+forma 2
+var header = document.querySelector('header');
+header.style.color = '#FF0000';
+*/
